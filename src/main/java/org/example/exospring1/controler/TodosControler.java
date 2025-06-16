2cd37621a5e5d427faed1db5561b00c64f134426
vivien <vivien@m2i.fr>
2025-06-16 15:22:18 +0200

    exo 1 minimum

--- src/main/java/org/example/exospring1/controler/TodosControler.java
+++ src/main/java/org/example/exospring1/controler/TodosControler.java
@@ -1,5 +1,6 @@
 package org.example.exospring1.controler;
 
+import org.example.exospring1.entity.Todos;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,31 +18,59 @@ public class TodosControler {
         return "todos";
     }
 
-    @RequestMapping("/home/listing")
+    @RequestMapping("/home/listingall")
     @ResponseBody
     public List<String> getTodos(){
         return List.of("Task 1", "Task 2", "Task 3", "Task 4");
     }
 
-    @RequestMapping("/home/listing")
+
+
+
+    @RequestMapping("/home/detaillast")
     public String todosInfos(Model model){
 model.addAttribute("name", "Task 1");
 model.addAttribute("description", "Relink Wire");
 model.addAttribute("status", true);
-List<String> todos = List.of("Task 1", "Task 2", "Task 3", "Task 4");
-model.addAttribute("todos", todos);
-
+        model.addAttribute("name", "Task 2");
+        model.addAttribute("description", "Scanner");
+        model.addAttribute("status", false);
+        model.addAttribute("name", "Task 3");
+        model.addAttribute("description", "Upload Manifold");
+        model.addAttribute("status", true);
         return "listing/details";
     }
 
-    @RequestMapping("/home/listing")
+//    @RequestMapping("/home/listingall")
+//    public String todosALL(Model model){
+//        model.addAttribute("name", "Task 1");
+//        model.addAttribute("description", "Relink Wire");
+//        model.addAttribute("status", true);
+//        List<String> todos = List.of("Task 1", "Task 2", "Task 3", "Task 4");
+//        model.addAttribute("todos", todos);
+//
+//        return "listing/listOFall";
+//    }
+
+
+//    @RequestMapping
+//    public Todos todos(){
+//        Todos todos = new Todos();
+//        todos.setName("Task 2");
+//        todos.setDescription("Upload Manifold");
+//        todos.setStatus(false);
+//        return
+//    }
+
+
+    @RequestMapping("/home/listingempty")
     public String todosInfosEmpty(Model model){
 //        model.addAttribute("name", "Task 1");
 //        model.addAttribute("description", "Relink Wire");
         List<String> todos = List.of();
         model.addAttribute("todos", todos);
 
-        return "listing/details";
+        return "listing/listOFall";
     }
 
 }
